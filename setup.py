df102d94196502bf74f2bfae7179c366e8ea1e94
Paul Schifferer <paul@schifferers.net>
2023-08-08 08:18:15 -0700

    Fix marshmallow-jsonapi version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name="sweetrpg-catalog-objects",
     install_requires=[
         "Flask~=2.0",
-        "marshmallow-jsonapi~=3.20",
+        "marshmallow-jsonapi~=0.24",
         "mongoengine~=0.27",
         "sweetrpg-api-core",
         "sweetrpg-db",
